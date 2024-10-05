e6fbd6965692089716b7be7f9b8a4393581cf06e
kiqf <139188372+kiqf@users.noreply.github.com>
2024-10-05 14:48:45 -0300

    view handlebar and a new route

--- routes/jobs.js
+++ routes/jobs.js
@@ -2,10 +2,22 @@ const express = require('express');
 const router = express.Router();
 const Job = require('../models/job');
 
+// Rota de Teste
 router.get('/test', (req, res) => {
     res.send('Welcome to the Job Finder');
 })
 
+// Detalhe da Vaga
+
+router.get('/view/:id', (req,res) => Job.findOne({
+    where: {id: req.params.id}
+}).then(job => {
+    res.render('view', {
+        job
+    });
+}).catch(err => console.log(err)));
+
+//Form da rota de envio
 router.get('/add', (req, res) => {
     res.render('add');
 })
